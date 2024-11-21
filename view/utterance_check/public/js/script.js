6a0c7a59b223f63dfd2b588b60cb304d4fa0bce4
Oda-2002 <167402905+Oda-2002@users.noreply.github.com>
2024-11-21 13:51:32 +0000

    2024/11/21 22:51

--- view/utterance_check/public/js/script.js
+++ view/utterance_check/public/js/script.js
@@ -83,7 +83,7 @@ document.addEventListener("DOMContentLoaded", function () {
         const transcript = event.results[0][0].transcript;
 
         // サーバーにリクエストを送信して、ひらがなと音素に変換
-        fetch('https://speechtraininggame.github.io/minigame/view/utterance_check/public/A.html', {
+        fetch('https://speechtraininggame-ntut.github.io/minigame-template/view/utterance_check/public/A.html', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
