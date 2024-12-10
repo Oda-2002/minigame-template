e37547d43bdf113eed8bbd45133e60fd7b27bbec
小田悠介 <odayu@MacBook-Air.local>
2024-12-11 07:01:39 +0900

    2024/12/11 7:01

--- app.py
+++ app.py
@@ -48,4 +48,4 @@ def index():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8080))
-    socketio.run(app, host='0.0.0.0', port=8080, debug=True)
+    socketio.run(app, host='0.0.0.0', port=port)
